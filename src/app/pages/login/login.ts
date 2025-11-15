08db562ac6e5e22d817bdb18d82ec2940b352247
gildevson <gildevson@gmail.com>
2025-11-15 18:24:42 -0300

    Adicioanndo mais dados

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class LoginComponent {}
